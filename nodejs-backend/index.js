dd089edfe2d2b29a30a074f45acdef69d5d4eb23
george <george.moore@cyted.ai>
2025-08-12 22:21:46 +0100

    add a basic GET /product endpoint

--- nodejs-backend/index.js
+++ nodejs-backend/index.js
@@ -2,6 +2,18 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+require('dotenv').config();
+const { fetchProducts, pool } = require("./modules/db.js")
+
+app.get("/product", async (req, res) => {
+    try {
+        const products = await fetchProducts();
+        res.json({ products });
+    } catch (err) {
+        console.error('Error fetching products:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Listening for connections on port ${PORT}`);
